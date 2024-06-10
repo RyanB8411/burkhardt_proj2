0853cf0b6e380c290621161e6a2b8183d3598c31
Ryan B <148020592+RyanB8411@users.noreply.github.com>
2024-06-10 13:51:54 -0400

    Finished commenting shape

--- src/main/java/com/project2/Shape.java
+++ src/main/java/com/project2/Shape.java
@@ -1,19 +1,23 @@
 /**Shape Class
  * 18May2024
  * Ryan Burkhardt
- * This class will be used to make a shape that passes in a simple int of number of dimensions
+ * This class will be used to make a shape that passes in a simple 4 variable shape
+ * It will be used in all of my shapes whether 2-d or 3-d
  */
 
 package com.project2;
 
 import javafx.scene.canvas.GraphicsContext;
 
+//Create an abstract class so it can be used for 3-d and 2-d shapes
+//The x and y will be the dimensions as well as the location on the canvas
 public abstract class Shape {
     private double x;
     private double y;
     private double width;
     private double height;
 
+    //Construct our shape with the setters
     public Shape(double x, double y, double width, double height) {
         this.x = x;
         this.y = y;
@@ -21,6 +25,7 @@ public abstract class Shape {
         this.height = height;
     }
 
+    //Provide all getters to use in our shape code.
     public double getX() {
         return x;
     }
